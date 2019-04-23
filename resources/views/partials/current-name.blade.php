5cb8749127f8ff9f65d1e5740868da060e9b1a95
Blair Wang <wzcblair@gmail.com>
2019-04-22 19:59:14 -0500

    Revert "Revert "add partials""
    This reverts commit 8e3bb2119f4f9274b863a5d2ff7047f46cf2d871.

---
+++ resources/views/partials/current-name.blade.php
@@ -0,0 +1,9 @@
+<div id="current-name" class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title">Your Name</h3>
+    </div>
+    <div class="panel-body">
+        <button id="edit-name-btn" class="btn" style="float:right" ><i class="fa fa-pencil"></i> Edit</button>
+        <p>{{$user->name}}</p>
+    </div>
+</div>
